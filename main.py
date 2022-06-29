0f348bc8555fe914d855282a47e85aa5f2e11355
NhanLe <lecongnhan29@gmail.com>
2022-06-29 04:29:45 +0000

    [add] loggings, 1000 candles, & receive new data

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 The main program starts here.
 """
 import os
-
+import logging
+import datetime
+import asyncio
 
 from dotenv import load_dotenv
 from my_binance import MyBinance
@@ -11,15 +13,47 @@ KEY_BINANCE_API_KEY = 'BINANCE_API_KEY'
 KEY_BINANCE_API_SECRET = 'BINANCE_API_SECRET'
 
 
-def main():
+async def main():
     """
     Main function
     """
+
+    # setups logging to a file named logs/[date]_[time].log and to the console
+    now = '{:%Y-%m-%d_%H-%M-%S}'.format(datetime.datetime.now())
+    # creates log file if it doesn't exist
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    logging.basicConfig(
+        filename=f'logs/{now}.log',
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    
+    # makes logging output to the console
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger().addHandler(console)
+
     api_key = os.getenv(KEY_BINANCE_API_KEY)
     api_secret = os.getenv(KEY_BINANCE_API_SECRET)
     binance = MyBinance(api_key, api_secret)
-    print(binance)
+    
+    # print(binance.get_k_lines('HNTUSDT'))
+
+    # subscribes to the symbols
+    await binance.subcribe_symbol("HNTUSDT")
+    await binance.subcribe_symbol("ZILTRY")
+
+    await binance.update()
+    
+    # TODO: predict the price
 
 if __name__ == '__main__':
     load_dotenv()
-    main()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
--- my_binance/__init__.py
+++ my_binance/__init__.py
@@ -4,11 +4,18 @@ Go to https://www.binance.com/en/my/settings/api-management for API Keys managem
 Binance constants: binance.client.Client
 """
 
+import asyncio
+import logging
 
 from binance.client import Client
-
+from binance.streams import BinanceSocketManager
 from my_binance.k_line import KLine
+from .symbols import SYMBOLS
 
+def handle_socket_message(msg):
+    print("ackj")
+    print(f"message type: {msg['e']}")
+    print(msg)
 
 class MyBinance:
     """
@@ -25,15 +32,61 @@ class MyBinance:
         self._api_key = api_key
         self._api_secret = api_secret
 
-        # inits the websocket manager
         self._client = Client(self._api_key, self._api_secret)
+        self._binance_socket_manager = BinanceSocketManager(self._client)
+        self._websockets = []
 
-        # gets data of the last 100 candles
-        for data in self._client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE,'1 hour ago UTC'):
-            k_line = KLine.from_list(data)
-            print(k_line)
+    def get_k_lines(self, symbol, interval:str=Client.KLINE_INTERVAL_1MINUTE, limit=1000):
+        """
+        Get the K Lines for a symbol
+
+        :param symbol: Symbol
+        :param limit: Limit
+        :return: List of KLine objects
+        """
+        logging.debug(
+            'getting k lines for %s at interval %s with limit %d',
+            symbol,
+            interval,
+            limit
+        )
+        if not symbol:
+            raise ValueError('Symbol is required')
+
+        if symbol not in SYMBOLS:
+            raise ValueError(f'Symbol {symbol} is not supported')
+
+        res = self._client.get_historical_klines(symbol, interval,'1 day ago UTC', limit=limit)
+        logging.debug('got %d K Lines for %s', len(res), symbol)
+        logging.debug(res)
+
+        return list(map(KLine.from_list, res))
+
+    async def subcribe_symbol(self, symbol:str):
+        """
+        Subscribe to a symbol
+
+        :param symbol: Symbol to subscribe
+        """
+        logging.debug('subscribing to %s', symbol)
+
+        if not symbol:
+            raise ValueError('Symbol is required')
+
+        if symbol not in SYMBOLS:
+            raise ValueError(f'Symbol {symbol} is not supported')
+
+        ws = self._binance_socket_manager.kline_socket(symbol, Client.KLINE_INTERVAL_1MINUTE)
+        await ws.connect()
+        self._websockets.append(ws)
+    
+    async def update(self):
+        while True:
+            # listens for all websockets in self._websockets
+            msgs = await asyncio.gather(*[ws.recv() for ws in self._websockets])
+            for msg in msgs:
+                handle_socket_message(msg)
+                
+            await asyncio.sleep(1)
 
 
-    def _handle_socket_message(self, msg):
-        print(f"message type: {msg['e']}")
-        print(msg)
